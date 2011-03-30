ce2e67a490406e12589dab9330f1a2af3dc84975
Dustin Mihalik <mihalik@gmail.com>
2011-03-30 15:35:19 -0500

    Adding Apache license headers to our files

--- js/ipad-app.js
+++ js/ipad-app.js
@@ -1,4 +1,21 @@
 /*
+ * Copyright 2010 Bazaarvoice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
 Function to turn our API response into something a little more friendly for use in Javascript Template languages.
 It recreates some of the heirarchy, handles a couple Null values for display, and makes a rating percent to use for stars.
 */
